872f23824cf8aa713d44b758ba78bb3f20bc397f
Mohammed <>
2020-08-18 16:55:35 +0200

    stage: navbar+forms

--- tweets/views.py
+++ tweets/views.py
@@ -1,13 +1,34 @@
+import random
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from .models import Tweet
+from .forms import TweetForm
 
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
     #return HttpResponse(f"<h1> Hello Afroze , {kwargs['tweet_id']} </h1>")
     return render(request, "pages/home.html", status = 200)
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid:
+        obj = form.save(commit=False)
+
+        obj.save()
+        form = TweetForm()
+    return render(request, 'components/form.html', context={"form" : form})
+
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{ "id" : x.id , "content" : x.content, "likes" : random.randint(0,100000)} for x in qs]
+    data = {
+        "isUser" : False,
+        "response" : tweets_list
+    }
+    return JsonResponse(data) 
 
 
 def tweet_detail_view(request, *args, **kwargs):
@@ -17,7 +38,7 @@ def tweet_detail_view(request, *args, **kwargs):
         return json data
     """
     data = {
-        "id" : kwargs['tweet_id']
+        "id" : Tweet.objects.get(id=kwargs['tweet_id']).id
     }
     status = 200
     try:
